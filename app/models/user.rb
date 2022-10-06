b761b7c4b95ea045e52ad4b9155a217ecf7672d7
Continuous-commit <fuki217@yahoo.co.jp>
2022-10-06 21:38:45 +0900

    始まり

---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  deleted_at             :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_one :profile, dependent: :destroy
+
+  delegate :name, :profile_text, :image, to: :profile, allow_nil: true
+
+  # 物理削除の代わりにユーザーの`deleted_at`をタイムスタンプで更新
+  def soft_delete  
+    update_attribute(:deleted_at, Time.current)  
+  end
+
+  # ユーザーのアカウントが有効であることを確認 
+  def active_for_authentication?  
+    super && !deleted_at  
+  end  
+
+  # 削除したユーザーにカスタムメッセージを追加します  
+  def inactive_message   
+    !deleted_at ? super : :deleted_account  
+  end 
+
+end
