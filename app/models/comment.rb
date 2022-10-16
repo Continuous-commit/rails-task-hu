38dcb34c6588a0fe2aa00ec09517b8d22b727d28
Continuous-commit <fuki217@yahoo.co.jp>
2022-10-16 17:36:07 +0900

    feat: add comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,24 +1,27 @@
 # == Schema Information
 #
-# Table name: tweets
+# Table name: comments
 #
 #  id         :integer          not null, primary key
 #  text       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  tweet_id   :integer          not null
 #  user_id    :integer          not null
 #
 # Indexes
 #
-#  index_tweets_on_user_id  (user_id)
+#  index_comments_on_tweet_id  (tweet_id)
+#  index_comments_on_user_id   (user_id)
 #
 # Foreign Keys
 #
-#  user_id  (user_id => users.id)
+#  tweet_id  (tweet_id => tweets.id)
+#  user_id   (user_id => users.id)
 #
-class Tweet < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  belongs_to :tweet
 
   validates :text, length: { maximum: 140 }, presence: true
 end
