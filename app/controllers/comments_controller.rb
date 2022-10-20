1e651636f212686f1209de01cd7920f38d1975dd
Continuous-commit <fuki217@yahoo.co.jp>
2022-10-20 21:24:38 +0900

    feat: コメント作成ページの作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+  before_action :find_tweet_id
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to tweet_path(@comment.tweet_id), notice: "コメントを投稿しました。"
+    else
+      flash.now[:alert] = 'コメントの投稿に失敗しました'
+      render new_tweet_comment_path(@tweet_id)
+    end
+  end
+
+  private
+
+  def find_tweet_id
+    @tweet_id = params[:tweet_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:tweet_id, :text)
+  end
+end
+
+
