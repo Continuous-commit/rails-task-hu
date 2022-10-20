f298742c97a3fe07f81ce9e0854bc20fb0837b4f
Continuous-commit <fuki217@yahoo.co.jp>
2022-10-20 21:24:38 +0900

    feat: コメント作成ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'comments/new'
+  get 'comments/create'
   root 'tweets#index'
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :profiles, only: [:show, :new, :edit, :create, :update]
-  resources :tweets
+  resources :tweets do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
