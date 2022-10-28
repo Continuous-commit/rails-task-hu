324739177c9243948634011360caa028311aa49c
Continuous-commit <fuki217@yahoo.co.jp>
2022-10-28 23:49:32 +0900

    feat: ActiveStorageの初期設定

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'kaminari'
 
 gem 'annotate'
 
-
+gem "image_processing", ">= 1.2"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :tweets, dependent: :destroy
   has_many :comments,  dependent: :destroy
+  has_one_attached :image
 
   delegate :name, :profile_text, :image, to: :profile, allow_nil: true
 
