aa7d697bf386dad7217c132c7b59c4fb1f414935
Continuous-commit <fuki217@yahoo.co.jp>
2022-11-13 10:16:00 +0900

    feat: omniauthマイグレーションファイルコメント削除

--- db/migrate/20221103000149_add_omniauth_to_users.rb
+++ db/migrate/20221103000149_add_omniauth_to_users.rb
@@ -2,7 +2,7 @@ class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :uid, :string
     add_column :users, :provider, :string, null: false, default: ""
-    # uidとproviderの組み合わせを一意にする
+    
     add_index :users, [:uid, :provider], unique: true
   end
 end
