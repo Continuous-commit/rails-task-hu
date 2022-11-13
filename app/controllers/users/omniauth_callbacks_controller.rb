7aa1107b78a4bc9e8728db199fb5921ac1920729
Continuous-commit <fuki217@yahoo.co.jp>
2022-11-13 09:54:16 +0900

    feat: profileコントローラとominiauth_callbackコントローラの修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,37 +9,37 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # request.env["omniauth.auth"]にGitHubから送られてきたデータが入っている
     @user = User.find_for_github_oauth(request.env["omniauth.auth"])
 
-    if @user.persisted? # データベースに保存されていればログイン成功
-      if @user.profile.blank?
-        sign_in @user, event: :authentication #プロフィールを作成していなければ、作成
-        redirect_to new_profile_path
-      else
-        sign_in_and_redirect @user, event: :authentication #プロフィールを作成していれば、ログイン
-      end
-      set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
-    else # ログイン失敗
+    unless @user.persisted?
       session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      return redirect_to new_user_registration_url
     end
+    
+    if @user.profile.blank?
+      sign_in @user, event: :authentication
+      return redirect_to new_profile_path
+    end
+    
+    sign_in_and_redirect @user, event: :authentication #プロフィールを作成していれば、ログイン
+    set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
   end
 
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
 
   # GET|POST /resource/auth/github
-  def passthru
-    super
-  end
+  # def passthru
+  #   super
+  # end
 
   # GET|POST /users/auth/github/callback
-  def failure
-    super
-  end
+  # def failure
+  #   super
+  # end
 
   protected
 
   # The path used when OmniAuth fails
-  def after_omniauth_failure_path_for(scope)
-    super(scope)
-  end
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
 end
