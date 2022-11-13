9bdd912bcaf272a80da0efc540eee21bd4c7439e
Continuous-commit <fuki217@yahoo.co.jp>
2022-11-13 10:08:53 +0900

    feat: ハッシュロケットの書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users, :controllers => { 
-    :registrations => "users/registrations",
-    :omniauth_callbacks => "users/omniauth_callbacks"
+  devise_for :users, controllers: { 
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   resources :profiles, only: [:show, :new, :edit, :create, :update]
