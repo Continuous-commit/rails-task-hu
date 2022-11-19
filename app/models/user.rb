f98096da1777a2dd90d09b9ffa37cfbb94e06cba
Continuous-commit <fuki217@yahoo.co.jp>
2022-11-19 15:47:26 +0900

    feat: dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,8 +47,8 @@ class User < ApplicationRecord
   
   has_one :profile, dependent: :destroy
   has_many :tweets, dependent: :destroy
-  has_many :comments,  dependent: :destroy
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   delegate :name, :profile_text, :image, to: :profile, allow_nil: true
 
