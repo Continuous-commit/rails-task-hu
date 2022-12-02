2e410082707dae3382fbace1ff8bc90f15c24bc5
Continuous-commit <fuki217@yahoo.co.jp>
2022-12-02 21:10:43 +0900

    feat: view, migrationファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(version: 2022_11_23_013527) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
