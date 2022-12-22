60d0f7e34fc6335d62a3504c4fb70bbab434fb11
Commit-continuous <fuki217@yahoo.co.jp>
2022-12-22 23:27:59 +0900

    feat: インスタンス変数非推奨

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -22,35 +22,35 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  before do
-    @profile = FactoryBot.build(:profile, name: 'テストくん', profile_text: 'This is the test')
-  end
+
+  let(:profile) {FactoryBot.build(:profile, name: 'テストくん', profile_text: 'This is the test')}
+  
 
   # 正常系
   describe 'type valid' do
     it '名前とプロフィール文とプロフィール画像があり、Userと紐づいている' do
-      @profile.image = fixture_file_upload('/files/test.jpg')
-      expect(@profile).to be_valid
+      profile.image = fixture_file_upload('/files/test.jpg')
+      expect(profile).to be_valid
     end
 
     it 'プロフィール画像がない場合はデフォルトの画像' do
-      @profile.save
-      expect(@profile.image).not_to eq nil
+      profile.save
+      expect(profile.image).not_to eq nil
     end
   end
 
   # 異常系
   describe 'type invalid' do
     it '名前がない' do
-      @profile.update(name: nil)
-      @profile.valid?
-      expect(@profile.errors[:name]).to include('を入力してください')
+      profile.update(name: nil)
+      profile.valid?
+      expect(profile.errors[:name]).to include('を入力してください')
     end
 
     it 'プロフィール文がない' do
-      @profile.update(profile_text: nil)
-      @profile.valid?
-      expect(@profile.errors[:profile_text]).to include('を入力してください')
+      profile.update(profile_text: nil)
+      profile.valid?
+      expect(profile.errors[:profile_text]).to include('を入力してください')
     end
   end
 end
