7bcb7a2ea82c8ceeee27f699808e8806c5b1a800
Commit-continuous <fuki217@yahoo.co.jp>
2022-12-27 22:20:54 +0900

    fix

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -24,36 +24,26 @@ RSpec.describe Tweet, type: :model do
   let(:tweet) { FactoryBot.build(:tweet, text: 'test tweet') }
 
   # 正常系
-  describe 'type valid' do
-    it 'ツイートにテキストがあり、ユーザーと紐づいている' do
-      expect(tweet).to be_valid
-    end
-
+  describe 'liked_by' do
     it 'ツイートをいいねできる' do
       user = FactoryBot.create(:user, email: 'user@example.com', password: 'password')
       tweet.save
       tweet.liked_by(user)
-      expect(Like.where(user: user)).to_not eq nil
+      expect(tweet.likes.find_by(user: user)).to_not eq nil
     end
   end
 
   # 異常系
   describe 'type invalid' do
-    it 'ツイートが空' do
-      tweet.update(text: nil)
-      tweet.save
-      expect(tweet.errors[:text]).to include('を入力してください')
-    end
-
     it 'ユーザーが紐づいていいない' do
       tweet = FactoryBot.build(:tweet, user: nil)
-      tweet.save
+      tweet.valid?
       expect(tweet.errors[:user]).to include('を入力してください')
     end
 
     it '140文字以上のツイート' do
-      tweet.update(text: 'a' * 141)
-      tweet.save
+      tweet.text = 'a' * 141
+      tweet.valid?
       expect(tweet.errors[:text]).to include('は140文字以内で入力してください')
     end
   end
